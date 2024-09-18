7287d8177121c625bbf6b95a1598c6895d52732f
Vishnuvardhanreddy bandi <124245030+Vishnureddy0404@users.noreply.github.com>
2024-09-18 13:54:20 +0530

    Create 3C.compare.cpp

---
+++ 3C.compare.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num1, num2;
+    std::cout << "Enter two integers: ";
+    std::cin >> num1 >> num2;
+    if (num1 == num2) {
+        std::cout << "The numbers are equal." << std::endl;
+    } else {
+        std::cout << "The numbers are not equal." << std::endl;
+    }
+    return 0;
+}
