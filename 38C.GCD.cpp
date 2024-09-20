35516e4a4646c8ca6a3af1dd60b5b41714220a77
Vishnuvardhanreddy bandi <124245030+Vishnureddy0404@users.noreply.github.com>
2024-09-20 09:10:42 +0530

    Create 38C.GCD.cpp

---
+++ 38C.GCD.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+int main() {
+    int num1, num2;
+    std::cout << "Enter two integers: ";
+    std::cin >> num1 >> num2;
+    std::cout << "GCD of " << num1 << " and " << num2 << " is: " << gcd(num1, num2) << std::endl;
+    return 0;
+}
